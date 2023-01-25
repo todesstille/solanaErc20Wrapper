063600eafcc632dcc0a78a7ae16e1eec45ff56f0
Dmitry Redkin <tanatonaut@gmail.com>
2023-01-25 15:58:13 +0600

    mint and transfer

--- programs/ERC20/src/lib.rs
+++ programs/ERC20/src/lib.rs
@@ -6,20 +6,33 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod erc20 {
     use super::*;
 
+    pub fn create_account(ctx: Context<CreateAccount>) -> Result<()> {
+        Ok(())
+    }
+
+
     pub fn mint(ctx: Context<Mint>, amount: u64) -> Result<()> {
         // Implement mint logic
         // Now it is mint on demand
         ctx.accounts.account.balance += amount;
         Ok(())
     }
+
+    pub fn transfer(ctx: Context<Transfer>, amount: u64) -> Result<()> {
+        require!(ctx.accounts.account1.balance >= amount, ERC20Error::InsufficientBalance);
+        ctx.accounts.account1.balance -= amount;
+        ctx.accounts.account2.balance += amount;
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Mint<'info> {
+pub struct CreateAccount<'info> {
     #[account(mut)]
     user: Signer<'info>,
     #[account(
-            init_if_needed,
+            init,
             payer = user,
             space = 8 + 8,
             seeds = [b"createAccount", user.key().as_ref()],
@@ -29,8 +42,33 @@ pub struct Mint<'info> {
     system_program: Program<'info, System>,
 }
 
+
+#[derive(Accounts)]
+pub struct Mint<'info> {
+    #[account(mut)]
+    user: Signer<'info>,
+    #[account(mut)]
+    account: Account<'info, TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Transfer<'info> {
+    #[account(mut)]
+    user: Signer<'info>,
+    #[account(mut)]
+    account1: Account<'info, TokenAccount>,
+    #[account(mut)]
+    account2: Account<'info, TokenAccount>,
+}
+
 #[account]
 #[derive(Default)]
 pub struct TokenAccount {
     balance: u64,
+}
+
+#[error_code]
+pub enum ERC20Error {
+    #[msg("Account has insufficient balance")]
+    InsufficientBalance,
 }
--- tests/ERC20.ts
+++ tests/ERC20.ts
@@ -3,13 +3,15 @@ import { Program } from "@project-serum/anchor";
 import { BN } from "bn.js";
 import { Erc20 } from "../target/types/erc20";
 import { Mock } from "@todesstille/mocksolana"
+import { assert } from "chai";
+import { AnchorError } from "@project-serum/anchor";
 
 const mock = new Mock(anchor);
 const provider = mock.getProvider();
 const program = anchor.workspace.Erc20 as Program<Erc20>;
 
 let admin, alice, bob;
-let aliceAccount;
+let aliceAccount, bobAccount;
 
 describe("ERC20", () => {
   
@@ -21,11 +23,20 @@ describe("ERC20", () => {
       [Buffer.from('createAccount'), alice.publicKey.toBuffer()],
       program.programId,
     );
-
+    bob = new anchor.web3.Keypair();
+    await mock.transfer(admin, bob.publicKey, 10000000);
+    [bobAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from('createAccount'), bob.publicKey.toBuffer()],
+      program.programId,
+    );
   });
 
-  it("Assets could be minted", async () => {
-    const tx = await program.methods.mint(new BN(123))
+  beforeEach(async () => {
+    await mock.waitBlock();
+  })
+
+  it("Could create account", async () => {
+    const tx = await program.methods.createAccount()
       .accounts({
         user: alice.publicKey,
         account: aliceAccount,
@@ -33,4 +44,53 @@ describe("ERC20", () => {
       .signers([alice])
       .rpc();
   });
+
+  it("Cant transfer with zero balance", async () => {
+    let tx = await program.methods.createAccount()
+      .accounts({
+        user: bob.publicKey,
+        account: bobAccount,
+      })
+      .signers([bob])
+      .rpc();
+    await mock.waitBlock();
+    let error;
+    try {
+      tx = await program.methods.transfer(new BN(1000))
+        .accounts({
+          user: alice.publicKey,
+          account1: aliceAccount,
+          account2: bobAccount,
+        })
+        .signers([alice])
+        .rpc();
+      } catch (err) {
+        error = err;
+        assert.isTrue(err instanceof AnchorError);
+        assert.equal(err.error.errorMessage, "Account has insufficient balance");
+      }
+      assert.ok(error != null);
+  });
+
+  it("Assets could be minted", async () => {
+    const tx = await program.methods.mint(new BN(1000))
+      .accounts({
+        user: alice.publicKey,
+        account: aliceAccount,
+      })
+      .signers([alice])
+      .rpc();
+  });
+
+  it("Could transfer", async () => {
+    let tx = await program.methods.transfer(new BN(1000))
+    .accounts({
+      user: alice.publicKey,
+      account1: aliceAccount,
+      account2: bobAccount,
+    })
+    .signers([alice])
+    .rpc();
+});
+
 });
